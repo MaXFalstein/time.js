71fff0bd25f273c359bdb55c7b99d0eb99bed83c
MaX Falstein <MaXFalstein@users.noreply.github.com>
2018-05-24 14:05:57 +0100

    Update time.js

--- time.js
+++ time.js
@@ -1,2 +1,8 @@
-var timeSpace = " ";
-var time = hour.concat(minute).concat(second).concat(milisecond);
+var d = new Date();
+var hours = d.getHours();
+var minutes = d.getMinutes();
+var seconds = d.getSeconds();
+var milliseconds = d.getMilliseconds();
+var colon = ":";
+var HMSColonTime = hours.concat(colon).concat(minutes).concat(colon).concat(seconds);
+var HMSMsColonTime = hours.concat(colon).concat(minutes).concat(colon).concat(seconds).concat(colon).concat(milliseconds);
